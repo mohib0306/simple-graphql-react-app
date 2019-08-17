9ffef68457de8376457a56452806c8fc52a8e1fb
mohib0306 <mohib0306.w@gmail.com>
2019-08-17 14:27:52 +0200

    Add first GraphQL query to fetch list of companies and show them
    CompanyList component will query compaines from the graphql server
    and present them in the list items
    
    Also added graphql-tag dependency to write graphql query and
    mutations

--- frontend/src/components/CompanyList/index.js
+++ frontend/src/components/CompanyList/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 import "./style.css";
 
 class CompanyList extends Component {
+  renderCompanies() {
+    return this.props.data.companies.map(company => {
+      return <li key={company.id}>{company.name} </li>;
+    });
+  }
   render() {
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
     return (
       <div className="list">
-        <div className="listHeader">Company List</div>
+        <div className="listHeader">Company List </div>
+        {this.renderCompanies()}
       </div>
     );
   }
 }
 
-export default CompanyList;
+const query = gql`
+  {
+    companies {
+      id
+      name
+    }
+  }
+`;
+export default graphql(query)(CompanyList);
