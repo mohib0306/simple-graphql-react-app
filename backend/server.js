0c40ed7ee7c16695fd04acdecedb58859d0feed1
mohib0306 <mohib0306.w@gmail.com>
2019-08-17 14:25:59 +0200

    Add cors dependency to solve Cross-origin resource sharing issue
    More details:
    - https://www.prisma.io/blog/enabling-cors-for-express-graphql-apollo-server-1ef999bfb38d
    - https://github.com/expressjs/cors/issues/134#issuecomment-413543241

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema/schema");
 
 const app = express();
-
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true,
-  pretty: true
-}));
+app.use(cors());
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+    pretty: true
+  })
+);
 app.listen(4000, () => {
-  console.log('Listening on port 4000. http://localhost:4000');
-})
+  console.log("Listening on port 4000. http://localhost:4000");
+});
